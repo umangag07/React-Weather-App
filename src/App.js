8c1d3568a992e03a98aa43cb6a23012beed2283c
umang <25umangagarwal@gmail.com>
2020-11-16 23:40:47 +0530

    Added the search bar functionality

--- src/App.js
+++ src/App.js
@@ -13,7 +13,11 @@ function App() {
      if (evt.key == "Enter"){
        fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
        .then(res => res.json())
-       .then(result =>setWeather(result))
+       .then(result =>{
+        setWeather(result);
+        setQuery('');
+        console.log(result)
+       })
      }
   }
   const dateBuilder =(d)=>{
@@ -33,6 +37,9 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..."
+            onChange={e =>setQuery(e.target.value)} 
+            value={query}
+            onKeyPress={search}
             />
         </div>
         <div className="location-box">
